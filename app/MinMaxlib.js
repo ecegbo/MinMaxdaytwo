2d1b915e409d57a71ba3d866a82ce6112b5b1f95
ecegbo <ecegbo@gmail.com>
2016-11-29 15:15:46 +0100

    first commit for minmax

---
+++ app/MinMaxlib.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+ findMinMax: function (myArray){
+	var min = myArray[0];
+	var max = myArray[0];
+
+	for(var i = 1; i < myArray.length; i++){
+		if (min > myArray[i]){
+			min = myArray[i];
+		}
+		else if(max < myArray[i]){
+			max = myArray[i];
+		}
+	}
+
+	if (min == max){
+		return [min];
+	}
+	else{
+		return [min, max];
+	}
+
+	
+} 
+}
